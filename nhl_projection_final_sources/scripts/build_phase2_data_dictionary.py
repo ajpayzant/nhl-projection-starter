b991415f57d133cd3a2a33423e4b1268d8190751
ajpayzant <ajpayzant@icloud.com>
2026-03-13 18:24:02 -0700

    Delete nhl_projection_final_sources directory

--- nhl_projection_final_sources/scripts/build_phase2_data_dictionary.py
+++
@@ -1,4 +0,0 @@
-from scripts.run_phase2e_reference_audit import main
-
-if __name__ == '__main__':
-    main()
--- nhl_projection_final_sources/scripts/run_debug_paths.py
+++
@@ -1,6 +0,0 @@
-from nhl_model.config import PATHS
-
-print('project_root:', PATHS.project_root)
-print('data_raw:', PATHS.data_raw)
-print('data_processed:', PATHS.data_processed)
-print('logs:', PATHS.logs)
